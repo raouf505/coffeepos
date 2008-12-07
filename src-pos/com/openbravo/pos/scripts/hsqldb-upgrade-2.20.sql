c5039c5e21408b5629f3fc85499db989dbf696d3
kirkfickert <kirkfickert@b4c5db24-c496-11dd-8e1c-c7436111cc0c>
2008-12-07 20:30:15 +0000

    initial import

---
+++ src-pos/com/openbravo/pos/scripts/hsqldb-upgrade-2.20.sql
@@ -0,0 +1,63 @@
+--    Openbravo POS is a point of sales application designed for touch screens.
+--    Copyright (C) 2008 Openbravo, S.L.
+--    http://sourceforge.net/projects/openbravopos
+--
+--    This program is free software; you can redistribute it and/or modify
+--    it under the terms of the GNU General Public License as published by
+--    the Free Software Foundation; either version 2 of the License, or
+--    (at your option) any later version.
+--
+--    This program is distributed in the hope that it will be useful,
+--    but WITHOUT ANY WARRANTY; without even the implied warranty of
+--    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+--    GNU General Public License for more details.
+--
+--    You should have received a copy of the GNU General Public License
+--    along with this program; if not, write to the Free Software
+--    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+-- Database upgrade script for HSQLDB
+
+-- v2.20 - v2.30
+
+INSERT INTO RESOURCES(ID, NAME, RESTYPE, CONTENT) VALUES('30', 'Printer.PartialCash', 0, $FILE{/com/openbravo/pos/templates/Printer.PartialCash.xml});
+
+CREATE TABLE _PRODUCTS_COM (
+    ID VARCHAR NOT NULL,
+    PRODUCT VARCHAR NOT NULL,
+    PRODUCT2 VARCHAR NOT NULL,
+    PRIMARY KEY (ID)
+);
+
+INSERT INTO _PRODUCTS_COM(ID, PRODUCT, PRODUCT2) SELECT CONCAT(PRODUCT, PRODUCT2), PRODUCT, PRODUCT2 FROM PRODUCTS_COM;
+
+ALTER TABLE PRODUCTS_COM DROP CONSTRAINT PRODUCTS_COM_FK_1; 
+ALTER TABLE PRODUCTS_COM DROP CONSTRAINT PRODUCTS_COM_FK_2; 
+DROP TABLE PRODUCTS_COM;
+
+CREATE TABLE PRODUCTS_COM (
+    ID VARCHAR NOT NULL,
+    PRODUCT VARCHAR NOT NULL,
+    PRODUCT2 VARCHAR NOT NULL,
+    PRIMARY KEY (ID),
+    CONSTRAINT PRODUCTS_COM_FK_1 FOREIGN KEY (PRODUCT) REFERENCES PRODUCTS(ID),
+    CONSTRAINT PRODUCTS_COM_FK_2 FOREIGN KEY (PRODUCT2) REFERENCES PRODUCTS(ID)
+);
+CREATE UNIQUE INDEX PCOM_INX_PROD ON PRODUCTS_COM(PRODUCT, PRODUCT2);
+
+INSERT INTO PRODUCTS_COM(ID, PRODUCT, PRODUCT2) SELECT ID, PRODUCT, PRODUCT2 FROM _PRODUCTS_COM;
+
+DROP TABLE _PRODUCTS_COM;
+
+ALTER TABLE TICKETS ADD COLUMN TICKETTYPE INTEGER DEFAULT 0 NOT NULL;
+DROP INDEX TICKETS_TICKETID;
+CREATE INDEX TICKETS_TICKETID ON TICKETS(TICKETTYPE, TICKETID);
+
+CREATE SEQUENCE TICKETSNUM_REFUND START WITH 1;
+CREATE SEQUENCE TICKETSNUM_PAYMENT START WITH 1;
+
+-- final script
+
+DELETE FROM SHAREDTICKETS;
+
+UPDATE APPLICATIONS SET NAME = $APP_NAME{}, VERSION = $APP_VERSION{} WHERE ID = $APP_ID{};
